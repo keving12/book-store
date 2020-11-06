e1c048cd02d14a2381bbd7f03d5dfc5975834a66
Kevin Gracie <kgracie12@gmail.com>
2020-11-06 14:35:11 +0000

    updating book reporting integration test

--- book-store/src/test/java/com/kgracie/mytutor/bookstore/web/BookReportControllerTest.java
+++ book-store/src/test/java/com/kgracie/mytutor/bookstore/web/BookReportControllerTest.java
@@ -1,33 +1,51 @@
 package com.kgracie.mytutor.bookstore.web;
 
-import com.kgracie.mytutor.reporting.api.BookReportingService;
+import com.kgracie.mytutor.sales.domain.Transaction;
+import com.kgracie.mytutor.sales.domain.TransactionType;
+import com.kgracie.mytutor.sales.repository.TransactionRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(BookReportController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class BookReportControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
     @MockBean
-    private BookReportingService bookReportingService;
+    private TransactionRepository transactionRepository;
 
     @Test
     void shouldPrintReport() throws Exception {
+        var transactions = List.of(
+                new Transaction.Builder()
+                        .transactionType(TransactionType.SALE)
+                        .title("Book B")
+                        .quantity(2)
+                        .value(64.00)
+                        .build()
+        );
+
+        given(transactionRepository.fetchTransactions()).willReturn(transactions);
+
         var result = mockMvc.perform(get("/book-store/report"))
                 .andReturn();
 
         var responseString = result.getResponse().getContentAsString();
 
-        assertThat(responseString).isEqualTo("");
+        assertThat(responseString).isEqualTo("Book B | 2 Copies Sold | £64.00 Total Profit\n");
     }
 }
